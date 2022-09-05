af727ae7d12ca16639a8b6b4a61a236bda1b1971
Kimjunghyunn <godqhr6846@daum.net>
2022-09-05 15:57:40 +0900

    feat: 이메일 전송 Controller 생성

--- src/main/java/shop/tryit/web/member/MemberController.java
+++ src/main/java/shop/tryit/web/member/MemberController.java
@@ -3,6 +3,7 @@ package shop.tryit.web.member;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
@@ -12,7 +13,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shop.tryit.domain.member.dto.EmailRequest;
 import shop.tryit.domain.member.dto.MemberFormDto;
 import shop.tryit.domain.member.service.MemberFacade;
 
@@ -55,6 +58,17 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    /**
+     * 이메일 전송
+     */
+    @PostMapping("/email")
+    public ResponseEntity<Void> authEmail(@RequestBody @Valid EmailRequest emailRequest) {
+        memberFacade.authEmail(emailRequest);
+        //성공했다면 200
+        return ResponseEntity.ok().build();
+    }
+
 
     /**
      * 로그인
